a2c9a2c6efa483746fce9de3d4461eb9351adeb2
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-19 10:46:33 +0200

    task-2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// отримуємо доступ до порожнього списку
+const listOfImages = document.querySelector('.gallery');
+
+// створюємо функцію для розмітки галереї
+
+const makeGalleryMarkup = image => {
+  const { preview, original, description } = image;
+
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const makeGallery = galleryItems.map(makeGalleryMarkup).join('');
+
+//  додаємо розмітку в список
+listOfImages.insertAdjacentHTML('afterbegin', makeGallery);
+
+listOfImages.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+new SimpleLightbox('.gallery a', { captionsData: 'alt', fadeSpeed: 250 });
 console.log(galleryItems);
