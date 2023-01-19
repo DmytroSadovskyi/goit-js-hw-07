34e010adf7cdaa16219501d0ce070017394d8de3
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-19 10:33:35 +0200

    task-1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// отримуємо доступ до div.gallery
+const listOfImages = document.querySelector('.gallery');
 
+// створюємо функцію для розмітки галереї
+
+const makeGalleryMarkup = image => {
+  const { preview, original, description } = image;
+
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+
+const makeGallery = galleryItems.map(makeGalleryMarkup).join('');
+
+//  додаємо розмітку в список
+listOfImages.insertAdjacentHTML('afterbegin', makeGallery);
+
+listOfImages.addEventListener('click', onGalleryClick);
+
+// реалізуємо делегування події на div.gallery та створення модалки з великим
+// зображенням всередині
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src="${e.target.dataset.source}">
+`
+  );
+  instance.show();
+}
 console.log(galleryItems);
